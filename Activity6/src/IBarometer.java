eca065f628f84689a3dee95b43218a9e38039c14
Bob <jm1132@g.rit.edu>
2020-02-16 12:45:00 +0100

    Fixing small tag bug

--- Activity6/src/IBarometer.java
+++
@@ -1,7 +0,0 @@
-public interface IBarometer {
-    final double MIN = 27.0; // minimum reading
-    final double MAX = 32.0; // maximum reading
-    final double DEFAULT = 29.92; // default reading.
-
-    public double pressure();
-}
